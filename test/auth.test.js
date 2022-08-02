d2a8a630a4d087294b2dfc7c21bf3a898edfc167
afrivera <andresrfelip@hotmail.com>
2022-08-01 21:41:22 -0500

    testing route auth with mocha and chai

--- test/auth.test.js
+++ test/auth.test.js
@@ -51,4 +51,67 @@ describe('Test Authenticate a user /auth', () => {
 
     })
 
+    it( 'POST [ERROR] - It should return a message with password required', async() => {
+
+        const { body } = await api.post('/api/auth/login')
+                                        .send( { email: 'usernoexist@test.com'} )
+                                        .expect('Content-Type', /json/)
+                                        .expect(403)
+                    
+        const { msg } = body.errors[0]
+        expect( msg ).to.be.a('string');
+        expect( msg ).to.equal('Password is required');
+
+    })
+
+    it( 'POST [ERROR] - It should return a message with email required', async() => {
+
+        const { body } = await api.post('/api/auth/login')
+                                        .send( { password: 'passwordTest'} )
+                                        .expect('Content-Type', /json/)
+                                        .expect(403)
+                    
+        const { msg } = body.errors[0]
+        expect( msg ).to.be.a('string');
+        expect( msg ).to.equal('Email is required');
+
+    })
+
+    it( 'POST [ERROR] - It should return an error with credentials invalid', async() => {
+
+        const res = await api.post('/api/auth/login')
+                                        .send( { password: 'passwordTest', email: 'emailtest@test.com'} )
+                                        // .expect('Content-Type', /application\/json/)
+                                        .expect(401)
+                    
+        expect( res.status).to.be.a('number').equal(401);
+        expect( res ).to.have.property('text').includes('Invalid Credentials');
+
+    })
+
+    it( 'GET [SUCCESS] - It should return a new token', async() => {
+
+        const { body } = await api.get('/api/auth/renew')
+                                        .set('x-token', token )
+                                        .expect('Content-Type', /application\/json/)
+                                        .expect(200)
+                    
+        const { code, status, message, body: bodyRes } = body;
+
+        expect( code ).to.be.a('number');
+        expect( code ).to.be.equal(200);
+        expect( status ).to.be.a('boolean');
+        expect( status ).to.equal(true);
+        expect( message ).to.be.a('string');
+        expect( message ).to.equal('renew token succesfully');
+        expect( bodyRes ).to.be.a('object');
+    
+        expect( bodyRes.user ).to.be.a('object');
+        expect( bodyRes.token ).to.be.a('string');
+        expect( bodyRes.menu ).to.be.a('array');
+    
+        token = bodyRes.token;
+
+    })
+
 })
