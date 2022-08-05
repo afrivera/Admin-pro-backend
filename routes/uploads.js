87184cb84a4690b847a824b51406e4c259009e14
afrivera <andresrfelip@hotmail.com>
2022-08-04 20:07:55 -0500

    fix fileupload

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,13 +3,13 @@
 */
 
 const { Router } = require('express');
-const expressFileUpload = require('express-fileUpload')
+const fileUpload = require('express-fileupload')
 const { fileUpload, getImage } = require('../controllers/uploads');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
 const router = Router();
 
-router.use( expressFileUpload() );
+router.use( fileUpload() );
 router.put('/:collection/:id', validateJWT, fileUpload)
 router.get('/:collection/:file',  getImage)
 
